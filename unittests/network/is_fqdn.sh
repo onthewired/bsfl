f3f19f0f47cf3983e0d56bd9f461aaff9c68f3b3
Gnarstick <gromly@hotmail.fr>
2015-08-11 14:07:36 +0000

    is_fqdn.sh modification

--- unittests/network/is_fqdn.sh
+++ unittests/network/is_fqdn.sh
@@ -35,23 +35,7 @@ errnum=0
 
 check "is_fqdn www.google.com" 0
 
-#Testing of a working real fqdn
-
-check "is_fqdn www.google.com." 0
-
 #Testing of a working fqdn
-
-check "is_fqdn localhost.local" 0
-
-#Testing of a working fqdn
-
-check "is_fqdn localhost.localdomain.localworld.localsolarsystem.localuniverse.local.localhost.localdomain.localworld.localsolarsystem.localuniverse.local.localhost.localdomain.localworld.localsolarsystem.localuniverse.local.localhost.localdomain.localworld.localsolarsystem.localhost.localdomain.localworld.localsolarsystem.localuniverse.local.localhost.localdomain.localworld.localsolarsystem.localuniverse.local.localhost.localdomain.localworld.localsolarsystem.localuniverse.local.localhost.localdomain.localworld.localsolarsy" 0
-
-#        The maximum numbet of octets is a label is 63. RFC1035.
-#		 The maximum number of octets in a domain name is 255 octets. RFC1035
-#        The maximum length of a fqdn, not a hostname, is 1004 characters.        RFC1035
-#        The maximum length of a full qualified hostname is 253 characters. Imputed from RFC952, RFC1123 and RFC1035.
-#		Please note that a real fqdn must en with a dot.
 
 check "is_fqdn localhost.local" 0
 
@@ -81,14 +65,6 @@ check "is_fqdn www.google-com.com" 0
 
 check "is_fqdn www.a./.com" 1
 
-#Testing of www.google-com.com
-
-check "is_fqdn www.google-com.com" 0
-
-#Testing of a buggy fqdn
-
-check "is_fqdn www.a./.com" 1
-
 #Testing of a buggy fqdn
 
 check "is_fqdn www.attr@ctiveworld.com" 1
@@ -105,20 +81,11 @@ check "is_fqdn www.\x41.com" 1
 
 check "is_fqdn azertyuiop" 1
 
-#Testing of www.google.com.com
-
-check "is_fqdn www.google.com.com" 1
-
-#Testing of ipv4
-
-check "is_fqdn 192.168.1.1" 1
-
 #Testing of ipv6
 
 check "is_fqdn 2001:db8:1f89:0:0:0:0:0" 1
 
 #Testing of an ip adress
-echo "Testing of an ip adress (128.128.128.128):"
 
 is_fqdn 128.128.128.128
 
@@ -127,8 +94,3 @@ check "is_fqdn 192.168.1.1" 1
 #Testing of ipv6
 
 conclude "is_fqdn"
-
-check "is_fqdn 2001:db8:1f89:0:0:0:0:0" 1
-
-conclude "is_fqdn"
-
