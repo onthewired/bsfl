05650b5ef78d704278d747c5b07b34d84d317b26
Gnarstick <gromly@hotmail.fr>
2015-08-11 13:39:59 +0000

    clean merge bug

--- unittests/network/is_fqdn.sh
+++ unittests/network/is_fqdn.sh
@@ -40,7 +40,6 @@ check "is_fqdn www.google.com" 0
 check "is_fqdn www.google.com." 0
 
 #Testing of a working fqdn
-<<<<<<< HEAD
 
 check "is_fqdn localhost.local" 0
 
@@ -54,8 +53,6 @@ check "is_fqdn localhost.localdomain.localworld.localsolarsystem.localuniverse.l
 #        The maximum length of a full qualified hostname is 253 characters. Imputed from RFC952, RFC1123 and RFC1035.
 #		Please note that a real fqdn must en with a dot.
 
-=======
-
 check "is_fqdn localhost.local" 0
 
 #Testing of a working fqdn
@@ -68,7 +65,6 @@ check "is_fqdn localhost.localdomain.localworld.localsolarsystem.localuniverse.l
 #        The maximum length of a full qualified hostname is 253 characters. Imputed from RFC952, RFC1123 and RFC1035.
 #		Please note that a real fqdn must en with a dot.
 
->>>>>>> b2fffcf87fff7da9ac3602ad5e668b3afe1e3097
 #Testing of a working fqdn (youtube)
 
 check "is_fqdn r5---sn-cg07luek.googlevideo.com" 0
@@ -76,7 +72,6 @@ check "is_fqdn r5---sn-cg07luek.googlevideo.com" 0
 #Testing of www.www.google.com
 
 check "is_fqdn www.www.google.com" 0
-<<<<<<< HEAD
 
 #Testing of www.google-com.com
 
@@ -86,8 +81,6 @@ check "is_fqdn www.google-com.com" 0
 
 check "is_fqdn www.a./.com" 1
 
-=======
-
 #Testing of www.google-com.com
 
 check "is_fqdn www.google-com.com" 0
@@ -96,7 +89,6 @@ check "is_fqdn www.google-com.com" 0
 
 check "is_fqdn www.a./.com" 1
 
->>>>>>> b2fffcf87fff7da9ac3602ad5e668b3afe1e3097
 #Testing of a buggy fqdn
 
 check "is_fqdn www.attr@ctiveworld.com" 1
@@ -118,7 +110,6 @@ check "is_fqdn azertyuiop" 1
 check "is_fqdn www.google.com.com" 1
 
 #Testing of ipv4
-<<<<<<< HEAD
 
 check "is_fqdn 192.168.1.1" 1
 
@@ -126,24 +117,18 @@ check "is_fqdn 192.168.1.1" 1
 
 check "is_fqdn 2001:db8:1f89:0:0:0:0:0" 1
 
-<<<<<<< HEAD
 #Testing of an ip adress
 echo "Testing of an ip adress (128.128.128.128):"
 
 is_fqdn 128.128.128.128
-=======
 
 check "is_fqdn 192.168.1.1" 1
->>>>>>> b2fffcf87fff7da9ac3602ad5e668b3afe1e3097
 
 #Testing of ipv6
 
-<<<<<<< HEAD
-=======
 conclude "is_fqdn"
->>>>>>> b2fffcf87fff7da9ac3602ad5e668b3afe1e3097
-=======
+
 check "is_fqdn 2001:db8:1f89:0:0:0:0:0" 1
 
 conclude "is_fqdn"
->>>>>>> b2fffcf87fff7da9ac3602ad5e668b3afe1e3097
+
